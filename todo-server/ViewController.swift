5143f85393ab7ad6b5669b302e99ad9e69a0031b
cccc4444 <kushlyansky244@gmail.com>
2022-05-10 10:50:02 +0300

    Initial Commit

---
+++ todo-server/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todo-server
+//
+//  Created by Danylo Kushlianskyi on 10.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
